088d5f2a5933df2bf72624b03e6d78421570e5af
takashiraki <stgtbike4649@gmail.com>
2026-04-16 23:01:43 +0000

    zigzag

--- .leetcode/6.zigzag-conversion.py
+++ .leetcode/6.zigzag-conversion.py
@@ -7,6 +7,22 @@
 # @lc code=start
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        
+        if numRows == 1:
+            return s
+        rows = [""] * numRows
+        cur_row = 0
+        going_down = True
+
+        for c in s:
+            rows[cur_row] += c
+            
+            if cur_row == numRows - 1:
+                going_down = False
+            elif cur_row == 0:
+                going_down = True
+
+            cur_row += 1 if going_down else -1
+
+        return "".join(rows)
 # @lc code=end
 
